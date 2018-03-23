8915c389acf863f49b20de071c6e1140b1404b59
Renato Neto <renatoneto@casebras.com.br>
2018-03-23 15:08:37 +0000

    queue adapter

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
+    config.active_job.queue_adapter = :async
+    
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              'mail.casebras.com.br',
